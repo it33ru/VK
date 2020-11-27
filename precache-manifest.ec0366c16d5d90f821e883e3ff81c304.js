dff716df33fc47da8247eee5d03f35418b5f9dca
it33ru <it33ru@ya.ru>
2020-11-27 11:52:02 +0000

    Updates

---
+++ precache-manifest.ec0366c16d5d90f821e883e3ff81c304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486ec876a3fc2230ad1de5820330c0b2",
+    "url": "/VK/index.html"
+  },
+  {
+    "revision": "d797f73e9c3db2c45170",
+    "url": "/VK/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "d797f73e9c3db2c45170",
+    "url": "/VK/static/js/2.0f723409.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/VK/static/js/2.0f723409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac93818fe0a5f03363c9",
+    "url": "/VK/static/js/main.d539d3cd.chunk.js"
+  },
+  {
+    "revision": "ab94136c70258823dcc4",
+    "url": "/VK/static/js/runtime-main.4731cef8.js"
+  }
+]);
