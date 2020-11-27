04cfb210d8dffee46e46991711906ca9c86b1666
it33ru <it33ru@ya.ru>
2020-11-27 07:41:56 +0000

    alfa 001

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/VK/precache-manifest.d6ba96462c72d3ab74a06062c4618ebb.js"
+  "/VK/precache-manifest.2d6fd327907efbc6a1fe14c2f2d6a1c2.js"
 );
 
 self.addEventListener('message', (event) => {
